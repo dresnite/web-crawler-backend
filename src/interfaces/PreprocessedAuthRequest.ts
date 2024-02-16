10b07401bb5b7d4e0ee81605ef8fc8e21242dadd
andresbytes <giantquartz@gmail.com>
2024-02-16 18:38:32 +0100

    Remove a small typo

--- src/interfaces/PreprocessedAuthRequest.ts
+++ src/interfaces/PreprocessedAuthRequest.ts
@@ -1,7 +1,6 @@
 import { Request } from "express";
 import IUser from "./IUser";
 
-// All of these values 
 export default interface PreprocessedAuthRequest extends Request {
     token?: string,
     user?: IUser
